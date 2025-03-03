df9414e3fbd0cc941f1e7518802198960caf252e
unstableme <unstableme02@gmail.com>
2025-03-03 18:36:44 +0545

    middleware updated

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XContentOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 
